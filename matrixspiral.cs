dd34887bc46580ef8625c550319845476d6abf7f
Syed Hussain Ather <shussainather@gmail.com>
2023-04-29 18:39:15 -0400

    position

--- matrixspiral.cs
+++ matrixspiral.cs
@@ -7,3 +7,12 @@ public class MatrixSpiral
         Console.Write("N = ");
         int n = int.Parse(Console.ReadLine());
         int[,] matrix = new int[n, n];
+        
+        FillMatrix(matrix, n);
+        PrintMatrix(matrix, n);
+    }
+
+    private static void FillMatrix(int[,] matrix, int n) 
+    {
+        int positionX = n / 2;
+        int positionY = n % 2 == 0 ? (n / 2) -1 : (n / 2);
