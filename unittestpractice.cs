f43402dc8c1bea1671c4d6d3e9e6d3c81b71399f
Syed Hussain Ather <shussainather@gmail.com>
2023-05-01 15:32:54 -0400

    sumtest

--- unittestpractice.cs
+++ unittestpractice.cs
@@ -6,3 +6,26 @@ public class SumatorTest
 {
   [TestMethod]
   public void SumTestTypicalCase()
+  {
+    int[] numbers = new int[] { 1, 2 };
+    long expected = 3;
+    long actual Sumator_Accessor.Sum(numbers)
+    Assert.AreEQual(expected, actual);
+  }
+
+  [TestMethod]
+  public void SumTestOverflow()
+  {
+    int[] numbers = new int[] { 2000000000, 2000000000 };
+    long expected = 4000000000;
+    long actual = Sumator_Accessor.Sum(numbers);
+    Assert.AreEqual(expected, actual);
+  }
+
+  [TestMethod]
+  [ExpectedException(typeof(NullReferenceException))]
+  public void SumTestNullArray()
+  {
+    Sumator_Accessor.Sum(null);
+  }
+}
