74f3332377397bd3010b3f3f09b2344b67b4882a
Syed Hussain Ather <shussainather@gmail.com>
2023-05-18 12:00:01 -0400

    init

---
+++ arduinocontrol.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class arduinoCtrl : MonoBehaviour
+{
+    // replace with your board's COM port
+    SerialPort stream = new SerialPort("COM52", 9600);
+
+    public Transform t;
+
+    void Start()
+    {
+        stream.Open();
+    }
+
+    void Update()
+    {
+        Vector3 lastData = Vector3.zero;
+
+        string UnSplitData = stream.ReadLine();
+        print(UnSplitData);
+        string[] SplitData = UnSplitData.Split('|');
+
+        float AccX = float.Parse(SplitData[1]);
+        float AccY = float.Parse(SplitData[2]);
+        float AccZ = float.Parse(SplitData[3]);
+
+        lastData = new Vector3(AccX, AccY, AccZ);
+
+        t.transform.rotation = Quaternion.Slerp(t.transform.rotation, Quaternion.Euler(lastData), Time.deltaTime * 2f);
+
+    }
+}
